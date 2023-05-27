8c982b1f34720fca135164610e08752d1e9e1d33
IgorMontezuma20 <igorminranda17@gmail.com>
2023-05-27 18:41:58 -0300

    feat: insert DTO's to city and state

--- src/address/dtos/returnAddress.dto.ts
+++ src/address/dtos/returnAddress.dto.ts
@@ -1,13 +1,16 @@
+import { returnCityDto } from 'src/city/dtos/returnCityDto.dto';
 import { AddressEntity } from '../entities/address.entity';
 
 export class ReturnAddressDto {
   complement: string;
   addressNumber: number;
   cep: string;
+  city?: returnCityDto;
 
   constructor(address: AddressEntity) {
     this.complement = address.complement;
     this.addressNumber = address.addressNumber;
     this.cep = address.cep;
+    this.city = address.city ? new returnCityDto(address.city) : undefined;
   }
 }
